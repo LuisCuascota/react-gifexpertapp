ea114e2eec61d63db3a07950028f983c076114bc
santy <cuascota442@gmail.com>
2021-08-09 18:53:56 -0500

    Actualizacion de https

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -1,6 +1,6 @@
 
 export const getGifts = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=nzzdopHRwYdJqjUiwi2ndUVOjnp0zQ6E&q=${encodeURI(category)}&limit=5`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=nzzdopHRwYdJqjUiwi2ndUVOjnp0zQ6E&q=${encodeURI(category)}&limit=5`
     const resp = await fetch(url);
     const { data } = await resp.json();
 
